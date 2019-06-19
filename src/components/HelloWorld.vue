60149060757c562eae3bb5e13bbd2d4c49cd08a6
CuiMingsheng <cuimingsheng119@163.com>
2019-06-19 23:07:03 +0800

    update

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,113 +1,97 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+    <div class="block">
+      <el-cascader
+        v-model="value"
+        :options="options"
+        @change="handleChange"
+        :props="props"
+      ></el-cascader>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+  export default {
+    data() {
+      return {
+        options:[],
+        value:"",
+        props:{
+          value:"bigRegionCode",
+          label:"bigRegionName",
+          children:"cities"
+        },
+        bigRegion:[
+          {
+            bigRegionCode:"rs1",
+            bigRegionName:"东北",
+            smallRegionCode:"",
+            smallRegionName:"",
+          },
+          {
+            bigRegionCode:"rs2",
+            bigRegionName:"华北",
+            smallRegionCode:"",
+            smallRegionName:"",
+          },
+          {
+            bigRegionCode:"rs3",
+            bigRegionName:"西北",
+            smallRegionCode:"",
+            smallRegionName:"",
+          },
+          {
+            bigRegionCode:"rs4",
+            bigRegionName:"华东",
+            smallRegionCode:"",
+            smallRegionName:"",
+          },
+          {
+            bigRegionCode:"rs5",
+            bigRegionName:"华中",
+            smallRegionCode:"",
+            smallRegionName:"",
+          },
+          {
+            bigRegionCode:"rs6",
+            bigRegionName:"华南",
+            smallRegionCode:"",
+            smallRegionName:"",
+          }
+        ],
+        smallRegion:[
+          {
+            bigRegionCode:"rs1",
+            bigRegionName:"东北",
+            smallRegionCode:"rs11",
+            smallRegionName:"吉林"
+          },
+          {
+            bigRegionCode:"rs1",
+            bigRegionName:"东北",
+            smallRegionCode:"rs12",
+            smallRegionName:"辽宁"
+          }
+        ],
+      }
+    },
+    mounted(){
+      this.initRegion();
+    },
+    methods: {
+      initRegion(){
+        this.options = this.bigRegion
+      },
+      handleChange(val) {
+        let value = val[0]
+        
+
+      }
     }
-  }
-}
+  };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
 </style>
